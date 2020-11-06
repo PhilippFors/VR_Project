0cb4fad8142a10ba375e4ea80075650ce566a61d
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-11-06 23:02:38 +0100

    Dragging designated objects, fix: $13g

---
+++ Assets/Scripts/DragTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragTest : Interactable
+{
+    bool reset = false;
+    public Dragging dragger;
+    public override void DragAction()
+    {
+        dragger.Drag(gameObject);
+
+    }
+
+    public override void HoldAction()
+    {
+        reset = false;
+        if (draggable)
+            DragAction();
+    }
+
+    public override void PointerClick()
+    {
+
+    }
+
+    public override void PointerEnter()
+    {
+        dragger.ogPos = transform.position;
+    }
+
+    public override void PointerExit()
+    {
+
+    }
+
+    public override void StopHold()
+    {
+        if (!dragger.onSurface)
+            reset = true;
+    }
+
+    void Update()
+    {
+        if (reset)
+            transform.position = Vector3.Lerp(transform.position, dragger.ogPos, Time.deltaTime * dragger.floatSpeed);
+
+        if (transform.position == dragger.ogPos & reset)
+        {
+            reset = false;
+            dragger.StopDrag(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Dragging.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragging : MonoBehaviour
+{
+    public Transform DragHelper;
+    public Vector3 ogPos;
+    public Camera cam;
+    Ray ray;
+
+    public float minDistance = 2f;
+    public float maxDistance = 5f;
+    public float floatSpeed = 0.5f;
+
+    public bool onSurface = false;
+    public void Drag(GameObject obj)
+    {
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        Vector3 newPos = Vector3.zero;
+        RaycastHit hit;
+        ray = new Ray(cam.transform.position, cam.transform.forward);
+        if (Physics.Raycast(ray, out hit, maxDistance, LayerMask.GetMask("Surface")))
+        {
+            newPos = hit.point;
+            onSurface = true;
+        }
+        else
+        {
+            newPos = cam.transform.position + cam.transform.forward * minDistance;
+            onSurface = false;
+        }
+        DragHelper.position = newPos;
+        obj.transform.rotation = Quaternion.Euler(0, 0, 0);
+        obj.transform.position = Vector3.Lerp(obj.transform.position, newPos, Time.deltaTime * floatSpeed);
+    }
+
+    public void StopDrag(GameObject obj)
+    {
+        obj.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+
+
+
+}
